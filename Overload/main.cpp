61ef6500ffe01407f6b2606ef6d7a16640ff1e99
RWHA05 <94277313+RWHA05@users.noreply.github.com>
2024-11-18 23:10:21 -0600

    Create main.cpp

---
+++ Overload/main.cpp
@@ -0,0 +1,14 @@
+#include "TEST.h"
+#include "data_define.h"
+
+int main()
+{
+    variantA vA;
+    variantB vB;
+    variantC vC;
+    
+    TEST object;
+    object.overload(vB);
+
+    return 0;
+}
